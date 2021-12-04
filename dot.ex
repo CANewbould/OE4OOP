51f5a0c1e8f5f59795551ab67f6b4c84411d0242
CANewbould <newbould391@btinternet.com>
2021-12-04 16:03:55 +0000

    v4.0.5.4

--- dot.ex
+++ dot.ex
@@ -9,13 +9,14 @@
 --= Application: dot.ex
 -- Description: a basic pre-processor for OE4OOP.
 ------
---[[[Version: 4.0.5.3
+--[[[Version: 4.0.5.4
 --Euphoria Versions: 4.0.5 upwards
 --Author: C A Newbould
---Date: 2021.10.30
+--Date: 2021.11.26
 --Status: incomplete; operational
 --Changes:]]]
---* extended decumentation
+--* added test for extension
+--* added prompt if source file not specified
 --
 --==Open Euphoria for OOP application: dot
 --
@@ -23,18 +24,25 @@
 --
 -- <instance>.<method>
 --
--- syntax. Confine your method calls to one-per-line.
--- When keyed file the code (I recommend using the OE convention for
--- the extension but prefixed by a 'd', so 'dex' or 'dexw'.
+-- syntax.
 --
---Run this application by keying:
--- <eucode>eui dot.ex <nameofsourcefile.dex[w]></eucode>
+-- Confine your method calls to one-per-line.
+-- When keyed, file the code (I recommend using the OE convention for
+-- the extension but prefixed by a 'd', so 'dex' or 'dexw').
+--
+-- Run this application by keying:
+-- <eucode>eui dot[.ex] <nameofsourcefile[.dex[w]]></eucode>
+--
+-- The default action is:
+-- <eucode>eui dot <nameofsourcefile></eucode>
+--
+-- as the pre-processor assumes an extension of //dex//.
 --
 -- A file //dottemp.ex// is created, being the converted OE4OOP code.
 -- This is then executed in OE as normal.
 --
 -- The pre-processor issues two information lines to the console before
--- directed the output.
+-- issuing the output.
 ------
 --*/
 --------------------------------------------------------------------------------
@@ -46,7 +54,7 @@
 --=== Includes
 --*/
 --------------------------------------------------------------------------------
-include std/console.e -- for any_key
+include std/console.e -- for any_key, prompt_string
 include std/search.e -- for begins
 include std/sequence.e -- for reverse
 include std/utils.e -- for iif
@@ -87,30 +95,20 @@ function transpose(sequence line)
     if begins("--", line) then return line end if
     integer dot = find(DOT, line)
     --if find, ('"', line) < dot then return line end if
-    if dot then
---show(dot, "dot found at %d\n")
+    if dot
+    then
         integer openquote = find(QUOTE, line, 1) -- v4.1.0.1
         integer closequote = find(QUOTE, line, dot) -- v4.1.0.1
         if openquote < dot and closequote then return line end if -- v4.1.0.1
         integer openbracket = find(OB, line, dot)
         integer closebracket = find(CB, line, dot)
---show(openbracket, "open-bracket found at %d\n")
         sequence fn = line[dot+1..openbracket-1]
---show(fn, "fn name is: %s\n")
         integer previousspace = find( SPACE, reverse(line[1..dot]))
-        if previousspace then previousspace = dot - previousspace + 1
+        if previousspace
+        then previousspace = dot - previousspace + 1
         else previousspace = 0
         end if
---show(previousspace, "previous space found at %d\n")
         sequence obj = line[previousspace+1..dot-1]
-/*
-show(obj, "obj = %s\n")
-show(line[1..previousspace] & fn & OB & obj, "first part = %s\n")
-show(openbracket, "( is at %d;")
-show(closebracket, ") is at %d\n")
-show(iif(closebracket > openbracket+1, COMMA, ""), "optional bit = %s\n")
-show(line[openbracket+1..$], "tail = %s") getc(0)
-*/
         return line[1..previousspace] & fn & OB & obj
                     & iif(closebracket > openbracket+1, COMMA, "")
                     & line[openbracket+1..$]
@@ -126,22 +124,33 @@ end procedure
 --------------------------------------------------------------------------------
 procedure main()
     sequence commline = command_line()
+    if length(commline) < 3
+    then commline = append(commline, prompt_string("File to be processed: "))
+    else --OK
+    end if
+    if find('.', commline[3])
+    then --OK
+    else commline[3] &= ".dex"
+    end if
     -- insert test for length and prompt if short
     filehandle ih = open(commline[3], "r") -- v4.0.5.2
-    if ih then --OK
+    if ih
+    then --OK
     else unopened(commline[3])
     end if
     sequence output = EMPTY_SEQUENCE
     while TRUE do
         object line = gets(ih)
-        if atom(line) then exit -- END_OF_STREAM
+        if atom(line)
+        then exit -- END_OF_STREAM
         else line = transpose(line) --do transformation
         end if
         output &= line
     end while
     close(ih)
     filehandle oh = open("dottemp.ex", "w")
-    if ih then puts(oh, output)--OK
+    if ih
+    then puts(oh, output)--OK
     else unopened("dottemp.ex")
     end if
     close(oh)
@@ -157,6 +166,14 @@ end procedure
 main()
 --------------------------------------------------------------------------------
 -- Previous versions
+--------------------------------------------------------------------------------
+--[[[Version: 4.0.5.3
+--Euphoria Versions: 4.0.5 upwards
+--Author: C A Newbould
+--Date: 2021.10.30
+--Status: incomplete; operational
+--Changes:]]]
+--* extended decumentation
 --------------------------------------------------------------------------------
 --[[[Version: 4.0.5.2
 --Euphoria Versions: 4.0.5 upwards
